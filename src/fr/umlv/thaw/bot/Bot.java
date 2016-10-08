6b3859e74824a060e35a0da5964de248a38dc9ab
Vahen <naeos@laposte.net>
2016-10-08 14:04:00 +0200

    * [ADD] Interface bot pour que chacun puisse créer son bot * [ADD] Gitbot * [ADD] GitHubBot * [ADD] RSSBot * [ADD] Thaw -> main * [UPD] build.properties

---
+++ src/fr/umlv/thaw/bot/Bot.java
@@ -0,0 +1,12 @@
+package fr.umlv.thaw.bot;
+
+/**
+ * Project :Thaw
+ * Created by Narex on 08/10/2016.
+ */
+public interface Bot {
+/*
+    TODO
+ */
+
+}
---
+++ src/fr/umlv/thaw/bot/GitBot.java
@@ -0,0 +1,11 @@
+package fr.umlv.thaw.bot;
+
+/**
+ * Project :Thaw
+ * Created by Narex on 08/10/2016.
+ */
+public class GitBot implements Bot {
+/*
+    TODO
+ */
+}
