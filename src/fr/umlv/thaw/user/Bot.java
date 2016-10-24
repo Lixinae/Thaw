d53fbb0f08a9fd2b3faa478411ccb4169289d9cf
klepage <killianlepage@gmail.com>
2016-10-24 13:17:40 +0200

    * [UPD]  Bot.java Ajout d'une petite definition de la classe (javadoc) + suppression de lignes mises en commentaires car ces champs ne sont plus contenus dans la classe elle meme mais plutot dans la classe abstraite AbstractUser

--- src/fr/umlv/thaw/user/Bot.java
+++ src/fr/umlv/thaw/user/Bot.java
@@ -1,17 +1,14 @@
 package fr.umlv.thaw.user;
 
 /**
- * Project :Thaw
- * Created by Narex on 12/10/2016.
+ * This class represent a bot that could be added in a channel.
  */
 public class Bot extends AbstractUser {
 
-    //    private final String name;
     private final String filePropertiesName;
 
     public Bot(String name, String filePropertiesName) {
         super(name);
-//        this.name = name;
         this.filePropertiesName = filePropertiesName;
     }
 
@@ -23,9 +20,7 @@ public class Bot extends AbstractUser {
                 '}';
     }
 
-    /**
-     * @return true is the user is a bot, false otherwise
-     */
+
     @Override
     public boolean isUserBot() {
         return true;
