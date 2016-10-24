6ddba3a68a392dddda2d02481bc78422a1f92229
klepage <killianlepage@gmail.com>
2016-10-24 13:51:44 +0200

    * [UPD]  Channel.java     Ajout d'une documentation de la classe     Changement des signatures de methodes pour avoir un User plutot qu'une String dans les methodes addMessageToQueue et delMessageFromQueue

--- src/fr/umlv/thaw/channel/Channel.java
+++ src/fr/umlv/thaw/channel/Channel.java
@@ -3,26 +3,29 @@ package fr.umlv.thaw.channel;
 import fr.umlv.thaw.user.Bot;
 import fr.umlv.thaw.user.User;
 
+/**
+ * This interface contains every method that is useful to implement a channel.
+ */
 public interface Channel {
 
     /**
      * This method add a message that has been wrote by a user in a certain date
      *
-     * @param nickname the nickname of the User
+     * @param user the user that send the message
      * @param date     the date in which the message has been received
      * @param message  the message that must been add
      * @return true if the message has been sent false otherwise
      */
-    boolean addMessageToQueue(String nickname, long date, String message);
+    boolean addMessageToQueue(User user, long date, String message);
 
     /**
      * Delete a message from the channel from the author at a certain instant
      *
-     * @param nickname the name of the author
+     * @param usr the user that sent the message at the given time
      * @param date     the date in which the message has been sent
      * @return true if the message has been removed, false otherwise
      */
-    boolean delMessageFromQueue(String nickname, long date);
+    boolean delMessageFromQueue(User usr, long date);
 
     /**
      * Add a user to the channel
