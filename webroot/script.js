c460c126bf22bfc29f5fc133106b4a68224da284
klepage <killianlepage@gmail.com>
2016-11-04 15:15:31 +0100

    * [UPD] script.js    -> Correction script selectChannel

--- webroot/script.js
+++ webroot/script.js
@@ -11,57 +11,57 @@ var username = "blork"
 // Envoie le message au serveur qui l'ajoutera à la base de donné du channel
 function sendMessage(){
 	var messageV= $("#textEntry");
-	
+
 	$.post("/sendMessage",JSON.stringify({channel : currentChannel, message : messageV.val(),username : username}),
 			function(){
-				
+
 //				element.append(data);
-			},"json")
+			},"json");
 }
 
 // Permet de creer un channel
 // Et envoie l'information au serveur
 function createChannel(){
-	
+
 	$.post("/createChannel",
 		function(data){
-			
-		}
+
+		});
 }
 
 function getListUsers(){
-	
-	
-	
+
+
+
 }
 
 // TODO A changer !
 function getListChannels(){
-	var listChannel = document.getElementById('listChannels')
+	var listChannel = document.getElementById('listChannels');
 	listChannel.children().remove();
-	
+
 	//var delimiter = "/"
 	// format : $.get(nomRequete,fonction a appliquer)
 	/*$.get("/getListChannels",function(data){
 			// Mettre un delimiteur entre chaque nom de channel pour split
 			// Exemple :
-			//   MonSuperChannel / UnAutreChannel / EncoreUn 
+			//   MonSuperChannel / UnAutreChannel / EncoreUn
 			var tab = data.split(delimiter)
 			tab.foreach(printElements)
 			for(val in tab){
-				
+
 			}
 		}
 	)*/
 	// TODO verifier fonctionnement
-	
+
 	// Va creer une liste cliquable avec chacun des channel
 	listChannel.append("<ul id=\"channels\" onclick=\"selectChannel()\">");
 	$.getJSON("/getListChannels",function(data){
 		$.each(data,function(val){
 			listChannel.append("<li> "+ val+" </li>");
-		}
-	}
+		})
+	});
 	listChannel.append("</ul>");
 }
 
@@ -69,19 +69,26 @@ function getListChannels(){
 function getListUsersFromChan(){
 	var usersListOnChan = document.getElementById('listUsers');
 	usersListOnChan.children().remove();
-	
+
 	usersListOnChan.append("<ul id=\"usersOnChan\">");
 	$.getJSON("/getListUsersForChanChannels/"+currentChannel,function(data){
 		$.each(data,function(val){
 			usersListOnChan.append("<li> "+ val+" </li>");
-		}
-	}
+		})
+	});
 	usersListOnChan.append("</ul>");
+}
+
+function getEventTarget(e) {
+	e = e || window.event;
+	return e.target || e.srcElement;
 }
 
 // Fonctionne
 function selectChannel(){
 	var ul = document.getElementById('channels');
+//	var ul = $("#channels");
+//	alert(ul.val());
 	ul.onclick = function(event) {
 		var target = getEventTarget(event);
 		//alert(target.innerHTML);
