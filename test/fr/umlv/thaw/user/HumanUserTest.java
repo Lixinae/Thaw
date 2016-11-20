d2b1ebb409bb7723da722940683995a02370f37a
Lepagekillian <killianlepage@gmail.com>
2016-11-20 10:22:38 +0100

    *[UPD] HumanUserTest.java Rajout des tests addChannel et delChannel, test ok

--- test/fr/umlv/thaw/user/HumanUserTest.java
+++ test/fr/umlv/thaw/user/HumanUserTest.java
@@ -1,12 +1,10 @@
 package fr.umlv.thaw.user;
 
+import fr.umlv.thaw.channel.ChannelImpl;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Project :Thaw
- * Created by Narex on 16/11/2016.
- */
+
 public class HumanUserTest {
 
     ////////// CONSTRUCTOR ///////////////
@@ -20,7 +18,7 @@ public class HumanUserTest {
         new HumanUser("truc");
     }
 
-    ////////// IS_USER_HUMAN ///////////////
+    ////////// IS_USER_HUMAN AND IS_USER_BOT///////////////
     @Test
     public void isUserHumanTrue() throws Exception {
         HumanUser humanUser = new HumanUser("test");
@@ -28,7 +26,7 @@ public class HumanUserTest {
     }
 
     @Test
-    public void isUserHumanFalse() throws Exception {
+    public void isUserBotFalse() throws Exception {
         HumanUser humanUser = new HumanUser("test");
         Assert.assertFalse(humanUser.isUserBot());
     }
@@ -50,14 +48,64 @@ public class HumanUserTest {
 
     ////////// ADD CHANNEL ///////////////
     @Test
-    public void addChannel() throws Exception {
-
+    public void addSingleChannel() throws Exception {
+        HumanUser doris = new HumanUser("Doris");
+        ChannelImpl nemo = new ChannelImpl(doris, "Nemo");
+        doris.addChannel(nemo);
+        Assert.assertTrue(doris.channels.contains(nemo));
     }
+
+    @Test
+    public void addMultipleChannel() throws Exception {
+        HumanUser pinot = new HumanUser("Pinot");
+        ChannelImpl nemo = new ChannelImpl(pinot, "Nemo");
+        ChannelImpl moris = new ChannelImpl(pinot, "Moris");
+        ChannelImpl didier = new ChannelImpl(pinot, "Didier");
+        pinot.addChannel(nemo);
+        pinot.addChannel(moris);
+        pinot.addChannel(didier);
+        Assert.assertTrue(pinot.channels.contains(nemo));
+        Assert.assertTrue(pinot.channels.contains(moris));
+        Assert.assertTrue(pinot.channels.contains(didier));
+    }
+
 
     ////////// DELETE CHANNEL ///////////////
     @Test
-    public void delChannel() throws Exception {
+    public void delSingleChannel() throws Exception {
+        HumanUser patrice = new HumanUser("Patrice");
+        ChannelImpl clement = new ChannelImpl(patrice, "Clement");
+        patrice.addChannel(clement);
+        Assert.assertTrue(patrice.channels.size() == 1);
+        Assert.assertTrue(patrice.delChannel(clement));
+        Assert.assertTrue(patrice.channels.isEmpty());
+    }
 
+    @Test
+    public void delMultipleChannel() throws Exception {
+        HumanUser lola = new HumanUser("Lola");
+        ChannelImpl monica = new ChannelImpl(lola, "Monica");
+        ChannelImpl juliette = new ChannelImpl(lola, "Juliette");
+        ChannelImpl julie = new ChannelImpl(lola, "Julie");
+        lola.addChannel(monica);
+        lola.addChannel(juliette);
+        lola.addChannel(julie);
+        Assert.assertTrue(lola.channels.size() == 3);
+        Assert.assertTrue(lola.delChannel(monica));
+        Assert.assertTrue(lola.channels.size() == 2);
+        Assert.assertTrue(lola.delChannel(julie));
+        Assert.assertTrue(lola.channels.size() == 1);
+        Assert.assertTrue(lola.delChannel(juliette));
+        Assert.assertTrue(lola.channels.isEmpty());
+    }
+
+    @Test
+    public void delSameChannel() throws Exception {
+        HumanUser patrick = new HumanUser("Patrick");
+        ChannelImpl clementine = new ChannelImpl(patrick, "Clementine");
+        patrick.addChannel(clementine);
+        Assert.assertTrue(patrick.delChannel(clementine));
+        Assert.assertFalse(patrick.delChannel(clementine));
     }
 
 }
