6e3ddf03ebcc3b40f318146081f7268a5feaecf3
Vahen <naeos@laposte.net>
2016-12-03 16:54:56 +0100

    [UPD] Script.js * Ajout des interval pour les fonctions getListMessageForChannel et getListChannel * Amelioration des requete avec done / fail / always

--- webroot/script.js
+++ webroot/script.js
@@ -13,10 +13,19 @@ $(document).ready(function(){
 			$("#currentUser").html(username);
 			$("#currentChannel").html(currentChannel);
 			initialize();
+			setReloadInterval();
 		});
 
 function initialize(){
 	textAreaDefaultValueDisappearOnClick();
+	getListChannels();
+	getListMessageForChannel();
+}
+
+function setReloadInterval(){
+
+    setInterval(getListChannels,500);
+    setInterval(getListMessageForChannel,600);
 }
 
 /* Permet d'avoir un texte par defaut dans la zone de d'ecriture de message et qui disparait lors du clique sur la zone */
@@ -39,22 +48,71 @@ function textAreaDefaultValueDisappearOnClick(){
 function sendMessage(){
     var  messageV = $('textArea#TextZone');
     //TODO : a jeter alert(messageV.val()); //pour verifier que j'ai bien recuperer la bonne chaine de caractere
-	$.post("/sendMessage",
-	    JSON.stringify({channel : currentChannel, message : messageV.val(),username : username}),
-		function(){
+	$.post("/api/sendMessage",
+	    JSON.stringify({channel : currentChannel, message : messageV.val(),username : username}))
+	    .done(function(response){
+            alert("sucess send message");
+            messageV.val("");
+	    })
+	    .fail(function(response){
+            alert("fail send message");
+		})
+		.always(function() {
+            alert( "finished" );
+        });
 
-//				element.append(data);
-		},"json");
 }
 
 // Permet de creer un channel
 // Et envoie l'information au serveur
+// TODO
 function createChannel(){
 
-	$.post("/createChannel",
-		function(data){
+	$.post("/api/createChannel",
+	    JSON.stringify({channelName:newChannelName,userName:username}))
+	    .done(function(response){
+            alert("sucess send message");
+        })
+        .fail(function(response){
+            alert("fail send message");
+        })
+        .always(function() {
+            alert( "finished" );
+        });
+}
 
-		});
+// TODO
+function deleteChannel(){
+
+	$.post("/api/deleteChannel",
+	    JSON.stringify({channelName:target,userName:username}))
+	    .done(function(response){
+            alert("sucess send message");
+        })
+        .fail(function(response){
+            alert("fail send message");
+        })
+        .always(function() {
+            alert( "finished" );
+        });
+}
+
+// TODO
+function getListMessageForChannel(){
+	var listChannel = $(".listChannels");
+	listChannel.children().remove();
+	
+	$.post("/api/getListMessageForChannel",
+			JSON.stringify({channelName:currentChannel})
+	    .done(function(response){
+            alert("sucess send message");
+        })
+        .fail(function(response){
+            alert("fail send message");
+        })
+        .always(function() {
+            alert( "finished" );
+        });
 }
 
 
@@ -63,51 +121,25 @@ function getListChannels(){
 	var listChannel = $(".listChannels");
 	listChannel.children().remove();
 
-	//var delimiter = "/"
-	// format : $.get(nomRequete,fonction a appliquer)
-	/*$.get("/getListChannels",function(data){
-			// Mettre un delimiteur entre chaque nom de channel pour split
-			// Exemple :
-			//   MonSuperChannel / UnAutreChannel / EncoreUn
-			var tab = data.split(delimiter)
-			tab.foreach(printElements)
-			for(val in tab){
-
-			}
-		}
-	)*/
-	// TODO verifier fonctionnement
-
 	// Va creer une liste cliquable avec chacun des channel
 	listChannel.append("<h2>List of Channels</h2>");
 	listChannel.append("<ul id=\"channels\" onclick=\"selectChannel()\">");
-	$.get("/api/getListChannels",
-        function(response){
-            $.each(response,function(key,val){
-                listChannel.append("<li> "+ val+" </li>");
+	$.get("/api/getListChannels")
+	        .done(function(response){
+                $.each(response,function(key,val){
+                    listChannel.append("<li> "+ val+" </li>");
+                });
+            })
+            .fail(function(response){
+                alert("fail send message");
+            })
+            .always(function() {
+                alert( "finished" );
             });
-        });
+        ;
 	listChannel.append("</ul>");
 }
 
-// Fonctionne
-function getListUsersFromChan(currentChannel){
-	var usersListOnChan = $(".listUsers");
-	usersListOnChan.children().remove();
-
-    usersListOnChan.append("<h2>ListUsers </h2>");
-	usersListOnChan.append("<ul id=\"usersOnChan\">");
-	$.post("/api/getListUserForChannel",
-	    JSON.stringify({channelName : currentChannel}),
-	    function(response){
-//	        alert(response);
-	        $.each(response,function(key,val){
-                usersListOnChan.append("<li> "+ val+" </li>");
-            });
-	    }
-    );
-	usersListOnChan.append("</ul>");
-}
 
 // Compatibilite pour IE si besoin
 function getEventTarget(e) {
@@ -131,17 +163,46 @@ function selectChannel(){
         else{
             return
         }
-
+		$("#currentChannel").html(currentChannel);
         $.post("/api/connectToChannel",
-            JSON.stringify({channelName : currentChannel,userName : username,oldChannelName : oldChannel}),
-            function (response){
-//                alert("alacon");
+            JSON.stringify({channelName : currentChannel,userName : username,oldChannelName : oldChannel}))
+            .done(function (response){
                 getListUsersFromChan(currentChannel);
+            })
+            .fail(function(response){
+                alert("fail send message");
+            })
+            .always(function() {
+                alert( "finished" );
             });
-//        alert(currentChannel);
 	});
-	
 }
+
+// Fonctionne
+function getListUsersFromChan(currentChannel){
+	var usersListOnChan = $(".listUsers");
+	usersListOnChan.children().remove();
+
+    usersListOnChan.append("<h2>ListUsers </h2>");
+	usersListOnChan.append("<ul id=\"usersOnChan\">");
+	$.post("/api/getListUserForChannel",
+	    JSON.stringify({channelName : currentChannel}))
+	    .done(function(response){
+            alert(response);
+            $.each(response,function(key,val){
+                usersListOnChan.append("<li> "+ val+" </li>");
+            });
+        })
+        .fail(function(response){
+            alert("fail send message");
+        })
+        .always(function() {
+            alert( "finished" );
+        });
+    );
+	usersListOnChan.append("</ul>");
+}
+
 //// TODO -> Test uniquement
 //function testAjax() {
 //		var username ="testify"
