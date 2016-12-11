5f7f46d593f261dbe55780f3f65ff6ca08ee1451
Lepagekillian <killianlepage@gmail.com>
2016-12-11 10:59:44 +0100

    * [UPD] Tools.java   Ajout de la documentation sur hashToSha256   Ajout d'une methode permettant de representer tableau de byte    (notre mot de passe encrypte) en format hexadecimal

--- src/fr/umlv/thaw/server/Tools.java
+++ src/fr/umlv/thaw/server/Tools.java
@@ -5,6 +5,7 @@ import fr.umlv.thaw.logger.ThawLogger;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -18,10 +19,31 @@ import java.util.logging.Level;
  */
 public class Tools {
 
+    /**
+     * This function take a String representation of a password and
+     * encrypt it with the SHA256 algorithm.
+     *
+     * @param password the password to encrypt
+     * @return the password encrypted with SHA256 algorithm
+     * @throws NoSuchAlgorithmException if the algorithm haven't been found
+     */
     public static byte[] hashToSha256(String password) throws NoSuchAlgorithmException {
         MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
         byte[] passBytes = password.getBytes(StandardCharsets.UTF_8);
         return sha256.digest(passBytes);
+    }
+
+    /**
+     * This function take a byte Array that represented an encrypted message
+     * by SHA256 protocol and return a String representation of the message
+     * in hexadecimal format.
+     * The format is always 64 bits long
+     *
+     * @param cryptedMessage message encrypted by SHA256 protocol
+     * @return a String representation of the encrypted message
+     */
+    public static String Sha256ToString(byte[] cryptedMessage) {
+        return String.format("%064x", new BigInteger(1, cryptedMessage));
     }
 
     static void answerToRequest(HttpServerResponse response, int code, Object answer, ThawLogger thawLogger) {
