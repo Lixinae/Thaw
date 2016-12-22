1a1694ab0d941ff4cb95c1b03ccd6653f7fe6751
Lepagekillian <killianlepage@gmail.com>
2016-12-22 12:54:06 +0100

    Ajout de justifications et doc diverses et variees (Channel package)
    * [UPD] ChannelFactory.java
    * [UPD] ChannelImpl.java

--- src/fr/umlv/thaw/channel/ChannelFactory.java
+++ src/fr/umlv/thaw/channel/ChannelFactory.java
@@ -2,6 +2,10 @@ package fr.umlv.thaw.channel;
 
 import fr.umlv.thaw.user.humanUser.HumanUser;
 
+/**
+ * This Class is a Static Factory
+ * for ChannelImpl.
+ */
 public class ChannelFactory {
 
     public static Channel createChannel(HumanUser creator, String channelName) {
