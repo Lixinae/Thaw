6043377f2badc73a12e80af30d62ce57cd6fd809
Lepagekillian <killianlepage@gmail.com>
2016-12-23 01:24:45 +0100

    Suppression commentaire inutile
    * [UPD] launch_server.sh

--- launch_server.sh
+++ launch_server.sh
@@ -3,4 +3,3 @@ java=$1
 echo "executing the server using $java"
 
 $java --add-exports java.base/sun.nio.ch=ALL-UNNAMED --add-exports java.base/sun.net.dns=ALL-UNNAMED  -jar dest/Thaw-1.0.jar
-#--add-exports=java.base/sun.security.tools.keytool=ALL-UNNAMED --add-exports=java.base/sun.security.x509=ALL-UNNAMED
