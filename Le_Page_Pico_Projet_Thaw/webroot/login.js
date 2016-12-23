2eeab8e02d7f4ff8ecf2388d9709de308d505676
Lepagekillian <killianlepage@gmail.com>
2016-12-23 08:48:28 +0100

    Ajout du dossier webroot pour l'archive de rendu
    * [UPD] Le_Page_Pico_Projet_Thaw

---
+++ Le_Page_Pico_Projet_Thaw/webroot/login.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function(){
+
+		});
+
+
+function connectToServer(){
+
+    var userName = $('#userNameEntry').val();
+    var password = $('#userPasswordEntry').val();
+
+	$.post("/api/connectToServer",
+	    JSON.stringify({userName:userName,password:password}))
+	    .done(function(response){
+            sessionStorage.userName = JSON.stringify(userName);//to memorize login through session
+			window.location.href = "./client/client.html";
+        })
+        .fail(function(response){
+            alert("fail connectToServer");
+        })
+        .always(function() {
+
+        });
+}
+function createLogin(){
+    var userName = $('#userNameEntry').val();
+    var password = $('#userPasswordEntry').val();
+    var patt = new RegExp("^[a-zA-Z][\\w]+$")//pattern that will be use by the server,
+	$.post("/api/createAccount",
+	    JSON.stringify({userName:userName,password:password}))
+	    .done(function(response){
+            alert("Login created. Now try to connect");
+        })
+        .fail(function(response){
+        if(! patt.test(userName)){
+        alert("username must contains only alphanumeric characters must begin with a letter and contains at least 2 characters");
+        }
+            alert("Impossible to create login.");
+        })
+        .always(function() {
+        });
+}
