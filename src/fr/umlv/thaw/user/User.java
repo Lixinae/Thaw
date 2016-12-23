3b7cfbeb1759872f58bcfe667dfadd711e7c37a9
Lepagekillian <killianlepage@gmail.com>
2016-12-23 03:03:28 +0100

    Nettoyage code
    * [UPD] User.java

--- src/fr/umlv/thaw/user/User.java
+++ src/fr/umlv/thaw/user/User.java
@@ -1,22 +1,11 @@
 package fr.umlv.thaw.user;
 
-import fr.umlv.thaw.channel.Channel;
-import fr.umlv.thaw.message.Message;
-
 /**
  * This interface represent a User that can be a
  * human or a bot
  */
 public interface User {
 
-//    /**
-//     * This method allow a HumanUser to send a message to a specific channel.
-//     *
-//     * @param message the message to send
-//     * @param chan    the Channel in which the message will be send
-//     * @return true if the message has been sent, false otherwise
-//     */
-//    boolean sendMessage(Channel chan, Message message);
 
     /**
      * @return the user name
